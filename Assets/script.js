33da9a945619769036bec38acd34a26570216722
Lizzle <leefboettcher@gmail.com>
2022-06-28 11:23:54 -0500

    updated functionality and results

--- Assets/script.js
+++ Assets/script.js
@@ -12,110 +12,47 @@ function generatePassword() {
 
   if (length > 128 || length < 8) {
     alert("Needs to meet specifications.")
-    return ""
+    
   }
 
-  var uppercase = confirm("Uppercase?");
-  var lowercase = confirm("Lowercase?")
-  var numerics = confirm("Numerics?")
+  var uppercase = confirm("Do you want Uppercase?");
+  var lowercase = confirm("Do you want Lowercase?")
+  var numerics = confirm("Do you want Numerics?")
   var specialCharacter = confirm("Provide a special character?")
   var passwordResult = "";
 
   length = Number(length);
 
-
   if (!uppercase && !lowercase && !numerics && !specialCharacter) {
     alert("Needs to select at least one character type.")
-    return generatePassword
-  } 
-  // // For 4 Positive options
-  // else if (uppercase && lowercase && numerics && specialCharacter) {
-  //   criteria = criteria.concat(uppercaseArr, lowercaseArr, numericsArr, specialCharacterArr)
-  // }
-  // // For 3 Positive options
-  // else if (uppercase && lowercase && numerics){
-  //   criteria = criteria.concat(uppercaseArr, lowercaseArr, numericsArr)
-  // }
-  // // For 3 Positive options
-  // else if (uppercase && lowercase && specialCharacter){
-  //   criteria = criteria.concat(uppercaseArr, lowercaseArr, specialCharacterArr)
-  // }
-  // // For 3 Positive options
-  // else if (uppercase && numerics && specialCharacter){
-  //   criteria = criteria.concat(uppercaseArr, numericsArr, specialCharacterArr)
-  // }
-  // // For 3 Positive options
-  // else if (uppercase && lowercase && numerics){
-  //   criteria = criteria.concat(uppercaseArr, lowercaseArr, numericsArr)
-  // }
-  // // For 2 Positive options
-  // else if (uppercase && lowercase){
-  //   criteria = criteria.concat(uppercaseArr, lowercaseArr)
-  // }
-  // // For 2 Positive options
-  // else if (uppercase && numerics){
-  //   criteria = criteria.concat(uppercaseArr, numericsArr)
-  // }
-  // // For 2 Positive options
-  // else if (uppercase && specialCharacter){
-  //   criteria = criteria.concat(uppercaseArr, specialCharacterArr)
-  // }
-  // // For 2 Positive options
-  // else if (lowercase && numerics){
-  //   criteria = criteria.concat(lowercaseArr, numericsArr)
-  // }
-  // // For 2 Positive options
-  // else if (lowercase && specialCharacter){
-  //   criteria = criteria.concat(lowercaseArr, specialCharacterArr)
-  // }
-  // // For 2 Positive options
-  // else if (numerics && specialCharacter){
-  //   criteria = criteria.concat(numericsArr, specialCharacterArr)
-  // }
-  // // For 1 Positive options
-  // else if (uppercase){
-  //   criteria = criteria.concat(uppercaseArr)
-  // }
-  // // For 1 Positive options
-  // else if (lowercase){
-  //   criteria = criteria.concat(lowercaseArr)
-  // }
-  // // For 1 Positive options
-  // else if (numerics){
-  //   criteria = criteria.concat(numericsArr)
-  // }
-  // // For 1 Positive options
-  // else if (specialCharacter) {
-  //   criteria = criteria.concat(specialCharacterArr)
-  // }
-  // else {
-  // }
-
+    
+  }
 
   if (uppercase) {
     criteria = criteria.concat(uppercaseArr)
-    console.log(criteria)
+    //console.log(criteria)
   }
 
   if (lowercase) {
     criteria = criteria.concat(lowercaseArr)
-    console.log(criteria)
+    //console.log(criteria)
   }
 
   if (numerics) {
     criteria = criteria.concat(numericsArr)
-    console.log(criteria)
+    //console.log(criteria)
   }
 
   if (specialCharacter) {
     criteria = criteria.concat(specialCharacterArr)
-    console.log(criteria)
+    //console.log(criteria)
   }
 
   var passWord = ("")
 
   for (var i = 0; i < length; i++) {
-    passWord = passWord + criteria[Math.floor(Math.random()*criteria.length)]
+    var passWord = passWord + criteria[Math.floor(Math.random()*criteria.length)]
+    //passwordResult = passwordResult.concat(passWord)
   }
 
   return passWord
@@ -132,3 +69,69 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+//Code not used.
+
+//   // For 4 Positive options
+//   else if (uppercase && lowercase && numerics && specialCharacter) {
+//     criteria = criteria.concat(uppercaseArr, lowercaseArr, numericsArr, specialCharacterArr)
+//   }
+//   // For 3 Positive options
+//   else if (uppercase && lowercase && numerics){
+//     criteria = criteria.concat(uppercaseArr, lowercaseArr, numericsArr)
+//   }
+//   // For 3 Positive options
+//   else if (uppercase && lowercase && specialCharacter){
+//     criteria = criteria.concat(uppercaseArr, lowercaseArr, specialCharacterArr)
+//   }
+//   // For 3 Positive options
+//   else if (uppercase && numerics && specialCharacter){
+//     criteria = criteria.concat(uppercaseArr, numericsArr, specialCharacterArr)
+//   }
+//   // For 3 Positive options
+//   else if (uppercase && lowercase && numerics){
+//     criteria = criteria.concat(uppercaseArr, lowercaseArr, numericsArr)
+//   }
+//   // For 2 Positive options
+//   else if (uppercase && lowercase){
+//     criteria = criteria.concat(uppercaseArr, lowercaseArr)
+//   }
+//   // For 2 Positive options
+//   else if (uppercase && numerics){
+//     criteria = criteria.concat(uppercaseArr, numericsArr)
+//   }
+//   // For 2 Positive options
+//   else if (uppercase && specialCharacter){
+//     criteria = criteria.concat(uppercaseArr, specialCharacterArr)
+//   }
+//   // For 2 Positive options
+//   else if (lowercase && numerics){
+//     criteria = criteria.concat(lowercaseArr, numericsArr)
+//   }
+//   // For 2 Positive options
+//   else if (lowercase && specialCharacter){
+//     criteria = criteria.concat(lowercaseArr, specialCharacterArr)
+//   }
+//   // For 2 Positive options
+//   else if (numerics && specialCharacter){
+//     criteria = criteria.concat(numericsArr, specialCharacterArr)
+//   }
+//   // For 1 Positive options
+//   else if (uppercase){
+//     criteria = criteria.concat(uppercaseArr)
+//   }
+//   // For 1 Positive options
+//   else if (lowercase){
+//     criteria = criteria.concat(lowercaseArr)
+//   }
+//   // For 1 Positive options
+//   else if (numerics){
+//     criteria = criteria.concat(numericsArr)
+//   }
+//   // For 1 Positive options
+//   else if (specialCharacter) {
+//     criteria = criteria.concat(specialCharacterArr)
+//   }
+//   else {
+//   }
